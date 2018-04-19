d33cae2daa74b08232c5dff553f492c24c2b4c4e
AARON TAM <S328154257@OCDSB>
2018-04-19 09:45:00 -0400

    Action

--- Action.py
+++ Action.py
@@ -25,11 +25,11 @@ SCREENHEIGHT = 400
 # The window is defined as (width, height), measured in pixels
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Snow")
+pygame.display.set_caption("Aaron Action")
 speed = 1
 SNOW = pygame.sprite.Group()
 
-for i in range(70):
+for i in range(120):
     mySnow = Snow(WHITE, 10,10, random.randint(5,20))
     mySnow.rect.x = random.randint(0,400)
     mySnow.rect.y = random.randint(0,400)
@@ -49,21 +49,24 @@ while carryOn:
         if event.type == pygame.QUIT: # Player clicked close button
             carryOn = False
 
-            
-
     # --- Game logic goes here
     # There should be none for a static image
     for snow in SNOW:
-     snow.moveBackward(10)
-    if snow.rect.y > SCREENHEIGHT:
-       snow.changeSpeed(random.randint(5,20))
-       snow.rect.y = -200
-       snow.rect.x = random.randint(0,400)                 
+        snow.moveBackward(8)
+        if snow.rect.y > SCREENHEIGHT:
+            snow.changeSpeed(random.randint(5,20))
+            snow.rect.y = -200
+            snow.rect.x = random.randint(0,400)                 
     # --- Draw code goes here
 
     # Clear the screen to white
     screen.fill(BLACK)
     SNOW.draw(screen)
+    # Queue different shapes and lines to be drawn
+    # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
+    # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+
     # Update the screen with queued shapes
     pygame.display.flip()
 
